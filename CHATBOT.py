76f82ba57087658481d8c5a2d1da8d8f0fd2c023
okk-ctrl <kanchansharma05@gmail.com>
2024-10-01 10:39:05 +0530

    Update CHATBOT.py

--- CHATBOT.py
+++ CHATBOT.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import os
-from utilis import get_answer, text_to_speech, autoplay_audio, speech_to_text 
+from utils import get_answer, text_to_speech, autoplay_audio, speech_to_text 
 from audio_recorder_streamlit import audio_recorder
 from streamlit_float import *
 
